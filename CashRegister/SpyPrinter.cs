ec20590d80326c9c9ea206dc29bd4ac2ef5fa990
hdeng <hdeng10@slb.com>
2022-10-26 14:56:20 +0800

    'test:Should_process_execute_printing'

---
+++ CashRegister/SpyPrinter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashRegister
+{
+    public class SpyPrinter : Printer
+    {
+        public bool Hasprint { get; set; }
+        public override void Print(string content)
+        {
+            base.Print(content);
+            Hasprint = true;
+            // send message to a real printer
+        }
+    }
+}
--- CashRegisterTest/CashRegisterTest.cs
+++ CashRegisterTest/CashRegisterTest.cs
@@ -9,11 +9,13 @@ namespace CashRegisterTest
 		public void Should_process_execute_printing()
 		{
 			//given
-			var cashRegister = new CashRegister();
+			var spyprinter = new SpyPrinter();
+			var cashRegister = new CashRegister(spyprinter);
 			var purchase = new Purchase();
 			//when
 			cashRegister.Process(purchase);
 			//then
+			Assert.True(spyprinter.Hasprint);
 			//verify that cashRegister.process will trigger print
 		}
 	}
